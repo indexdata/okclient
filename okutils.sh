7280f5de9491fba5248de8bed808ccd17e380322
nielserik <nielserik@indexdata.com>
2024-04-20 13:33:54 +0200

    "join" util: some input validation

--- okutils.sh
+++ okutils.sh
@@ -1,17 +1,20 @@
 
 function OKJoin() {
   local api=$1; local condition=$2; local identifiers=$3;
-  arrayName="$(OK "$api" -j "keys[] as \$k | select(\"\\(.[\$k] | type)\"==\"array\") | \"\\(\$k)\" ")"
-  splicedJson="{ \"$arrayName\": [] }"
-  lineCount=$(echo -n "$identifiers" | grep -c '^')
-  for (( fromLine=1, pageSize=100; fromLine<=lineCount; fromLine=fromLine+pageSize )); do
-    page="$(echo "$identifiers" | sed -n "$(( fromLine )),$(( fromLine+pageSize-1 ))p;$(( fromLine+pageSize ))q")"
-    identifierQuery="$condition(${page//$'\n'/ OR })"
-    records="$(OK "$api" -n -q "$identifierQuery" -j 'RECORDS[]')"
-    splicedJson="$(printf "%s" "$splicedJson" "$records" | jq ".$arrayName += [inputs]")"
-  done
-  splicedJson="$(printf "%s" "$splicedJson" | jq ".$arrayName |= unique_by(.id) | .totalRecords=(.$arrayName|length)")"
-  printf "%s\n" "$splicedJson"
+  if arrayName="$(_get_name_of_array "$api")" ; then
+    splicedJson="{ \"$arrayName\": [] }"
+    lineCount=$(echo -n "$identifiers" | grep -c '^')
+    for (( fromLine=1, pageSize=100; fromLine<=lineCount; fromLine=fromLine+pageSize )); do
+      page="$(echo "$identifiers" | sed -n "$(( fromLine )),$(( fromLine+pageSize-1 ))p;$(( fromLine+pageSize ))q")"
+      identifierQuery="$condition(${page//$'\n'/ OR })"
+      records="$(OK "$api" -n -q "$identifierQuery" -j 'RECORDS[]')"
+      splicedJson="$(printf "%s" "$splicedJson" "$records" | jq ".$arrayName += [inputs]")"
+    done
+    splicedJson="$(printf "%s" "$splicedJson" | jq ".$arrayName |= unique_by(.id) | .totalRecords=(.$arrayName|length)")"
+    printf "%s\n" "$splicedJson" && return 0
+  else
+    printf "Error: %s\n" "$arrayName" && return 1
+  fi
 }
 
 function OKSliceAndSplice() {
@@ -24,5 +27,13 @@ function OKSliceAndSplice() {
     splicedJson="$($splicingFunction "$sliceOfTokens" "$splicedJson")";
   done
   printf "%s" "$splicedJson"
+}
+
+function _get_name_of_array () {
+  api=$1
+  arrayName="$(OK "$api" -j "keys[] as \$k | select(\"\\(.[\$k] | type)\"==\"array\") | \"\\(\$k)\" ")"
+  [[ ! "$(echo -n "$arrayName" | grep -c '^')" -eq 1 ]] && printf "'%s' not an API collection request? \nExpected a single array, found: <%s>\n" "$api" "$arrayName" && return 1
+  echo "$arrayName"
+  return 0
 }
 
