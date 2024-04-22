c62f4be7bc510ee10bd3cfb9618d73ab00f08044
nielserik <nielserik@indexdata.com>
2024-04-22 10:28:57 +0200

    Support sessions when paging, set page size 50.
      - support sessions with ok_join and ok_get_name_of_array
      - reduce page size from 100 to 50 in ok_join and ok_slice_and_splice (100 identifiers with long names can make OR query exceed URL max length)

--- okutils.sh
+++ okutils.sh
@@ -1,13 +1,13 @@
 
 function ok_join() {
-  local api=$1; local condition=$2; local identifiers=$3;
-  if arrayName="$(ok_get_name_of_array "$api")" ; then
+  local api=$1; local condition=$2; local identifiers=$3; local session="$4"
+  if arrayName="$(ok_get_name_of_array "$api" "$session")" ; then
     splicedJson="{ \"$arrayName\": [] }"
     lineCount=$(echo -n "$identifiers" | grep -c '^')
-    for (( fromLine=1, pageSize=100; fromLine<=lineCount; fromLine=fromLine+pageSize )); do
+    for (( fromLine=1, pageSize=50; fromLine<=lineCount; fromLine=fromLine+pageSize )); do
       page="$(echo "$identifiers" | sed -n "$(( fromLine )),$(( fromLine+pageSize-1 ))p;$(( fromLine+pageSize ))q")"
       identifierQuery="$condition(${page//$'\n'/ OR })"
-      records="$(OK "$api" -n -q "$identifierQuery" -j 'RECORDS[]')"
+      records="$(OK -S "$session" "$api" -n -q "$identifierQuery" -j 'RECORDS[]')"
       splicedJson="$(printf "%s" "$splicedJson" "$records" | jq ".$arrayName += [inputs]")"
     done
     splicedJson="$(printf "%s" "$splicedJson" | jq ".$arrayName |= unique_by(.id) | .totalRecords=(.$arrayName|length)")"
@@ -22,7 +22,7 @@ function ok_slice_and_splice() {
   local splicingFunction=$2
   local splicedJson=$3
   lineCount=$(echo -n "$allTokens" | grep -c '^')
-  for (( fromLine=1, pageSize=100; fromLine<=lineCount; fromLine=fromLine+pageSize )); do
+  for (( fromLine=1, pageSize=50; fromLine<=lineCount; fromLine=fromLine+pageSize )); do
     sliceOfTokens="$(echo "$allTokens" | sed -n "$(( fromLine )),$(( fromLine+pageSize-1 ))p;$(( fromLine+pageSize ))q")"
     splicedJson="$($splicingFunction "$sliceOfTokens" "$splicedJson")";
   done
@@ -31,7 +31,8 @@ function ok_slice_and_splice() {
 
 function ok_get_name_of_array () {
   api=$1
-  arrayName="$(OK "$api" -j "keys[] as \$k | select(\"\\(.[\$k] | type)\"==\"array\") | \"\\(\$k)\" ")"
+  session=$2
+  arrayName="$(OK -S "$session" "$api" -j "keys[] as \$k | select(\"\\(.[\$k] | type)\"==\"array\") | \"\\(\$k)\" ")"
   [[ ! "$(echo -n "$arrayName" | grep -c '^')" -eq 1 ]] && printf "'%s' not an API collection request? \nExpected a single array, found: <%s>\n" "$api" "$arrayName" && return 1
   echo "$arrayName"
   return 0
